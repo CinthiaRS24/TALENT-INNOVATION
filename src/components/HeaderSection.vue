44cbef683986b181eab0aaef68bbe04ff3409aeb
Cinthia Ramos <cinthia24027@gmail.com>
2022-07-08 10:16:01 -0500

    improve HeaderSection

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -1,34 +1,50 @@
 <template>
-<!-- <v-container>
-    <v-layout>
-    <v-flex> -->
-        <v-app-bar color="primary" dark>
-            <v-app-bar-title>Talent Innovation</v-app-bar-title>
+    <v-app-bar color="primary" dark>
+        <v-img
+            max-width="70"
+            src="../assets/logoTI.png"
+        >
+        </v-img>
 
-            <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-     
         <v-btn  
             color="secondary"
             elevation="2"
-            depressed >
-            Usuario Conectado
-            <v-icon
-                dark
-                right
+            depressed
+        >
+            <span class="text-capitalize pr-3 font-weight-light">{{user.displayName}}</span>
+            <v-img
+                height="30"
+                width="30"
+                pl="18"
+                src="../assets/user.png"
             >
-                favorite
-            </v-icon>
+            </v-img>
         </v-btn>
 
-        <p>Notif</p>
-        </v-app-bar>
-
+        <v-btn
+            color="primary"
+            elevation="0"
+        >
+            <v-img
+                max-width="30"
+                src="../assets/notificacion.png"
+            >
+            </v-img>
+        </v-btn>
+    </v-app-bar>
 </template>
 
 <script>
+import firebase from 'firebase/compat/app';
 
 export default {
     name: 'HeaderSection',
+     data() {
+      return {
+        user: firebase.auth().currentUser
+      }
+    }
 }
 </script>
