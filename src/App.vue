f42e2341cd5da641f4509a47a1c188033c20ff79
Cinthia Ramos <cinthia24027@gmail.com>
2022-07-08 20:13:00 -0500

    Start using router-view properly (remove direct components on the App.vue)

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <template>
-  <div>
+  <div id="app">
     
-    <div id="app">
-      <v-app style="background: #002855;">
-        <LoginView v-if="!user"></LoginView>
-        <!-- <LoginView v-else></LoginView> -->
-        <router-view/>
-    </v-app>
-      
-    </div>
+    <v-app style="background: #002855;">
+      <router-view />
+    </v-app>      
+    
   </div>
 </template>
 
 <script>
-import LoginView from './views/LoginView.vue'
-import firebase from 'firebase/compat/app';
-
 export default {
-  name: 'App',
-  components: {
-    LoginView
-  },
-  data() {
-    return {
-      user: firebase.auth().currentUser
-    }
-  }
+  name: 'App'
 }
 </script>
