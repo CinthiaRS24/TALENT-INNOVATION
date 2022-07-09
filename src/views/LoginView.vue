328fcc0c545a1dd85b14f48060cbff06d593ac2c
Cinthia Ramos <cinthia24027@gmail.com>
2022-07-09 18:49:02 -0500

    Improve LoginView styles

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,15 +2,24 @@
     <v-layout row wrap align-center>
         <v-flex sm4 offset-sm4>
             <v-card class="d-flex align-center justify-center" height="500" width="380">
-                <v-card-title primary-title class="mb-5 pb-5 ml-6 mr-6 align-center justify-center">
+                <v-card-title primary-title class="mb-3 pb-3 ml-6 mr-6 align-center justify-center">
                         <p class="display-1 font-weight-black primary--text">
                             Talent Innovation
                         </p>
                         
-                        <p class="body-2 mb-15 pb-15 font-weight-black primary--text">
+                        <p class="body-2 mb-5 pb-5 font-weight-black primary--text">
                             Esto será una asombrosa experiencia
                         </p>
-                        
+
+                        <div >
+                            <v-img
+                                class="mt-4"
+                                width="140"
+                                src="../assets/kami.png"
+                            >
+                            </v-img>
+                        </div>
+                                    
                         <v-btn 
                             class="mt-15" 
                             color="secondary"
@@ -18,8 +27,12 @@
                             depressed 
                             @click="googleLogin"
                             >
-
-                            Ingresa con Google
+                            <v-img
+                                width="40"
+                                src="../assets/google.png"
+                            >
+                            </v-img>
+                            <span class="pr-2">Ingresa con Google</span>
                         </v-btn>
                 </v-card-title>
             </v-card>
