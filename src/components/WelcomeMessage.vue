23a742c40f0e49b6555627cb2d4ab192ab1b93db
Cinthia Ramos <cinthia24027@gmail.com>
2022-07-10 15:14:00 -0500

    Improve styles and fix code indentation in WelcomeMessage

--- src/components/WelcomeMessage.vue
+++ src/components/WelcomeMessage.vue
@@ -1,9 +1,26 @@
 <template>
-    <div>
-        <img src="https://economipedia.com/wp-content/uploads/robot1-1.jpg" width="30" alt="Bot avatar" />
-        <p class="white--text">¡Hola soy Kamy!</p>
-        <p class="white--text">Escribe brevemente cuál es tu consulta</p>
-    </div>
+    <v-container class="d-flex align-start" width="80">
+        <v-avatar size="82px" class="mt-2">
+            <img
+                src="../assets/kami.png"
+                alt="Avatar"
+            >
+        </v-avatar>
+
+        <v-container class="pl-0">
+            <v-card color="primary" class="rounded-lg pr-2" style="width:max-content;">
+                <p class="subtitle white--text text-start font-weight-light ml-2 mb-3 pt-1 pb-1">
+                    ¡Hola soy Kamy!
+                </p>
+            </v-card>
+            
+            <v-card color="primary" class="rounded-lg pr-2" style="width:max-content;">
+                <p class="subtitle-2 white--text font-weight-light ml-2 pt-1 pb-1">
+                    Escribe brevemente cuál es tu consulta
+                </p>
+            </v-card>
+        </v-container>
+    </v-container>
 </template>
 
 <script>
@@ -11,7 +28,3 @@ export default {
     name: "WelcomeMessage"
 }
 </script>
-
-<style>
-
-</style>
