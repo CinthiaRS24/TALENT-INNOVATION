19d231511eb1d9e9ee3db62dee8080f62e0b03d8
Cinthia Ramos <cinthia24027@gmail.com>
2022-07-10 21:19:20 -0500

    Improve styles and fix code indentation in CoursesSection

--- src/components/CoursesSection.vue
+++ src/components/CoursesSection.vue
@@ -1,16 +1,16 @@
 <template>
-    <v-container>
-        <p class="white--text">Cursos</p>
+    <v-container class="pb-0">
 
-        <v-container class="d-flex justify-space-around" >
+        <p class="white--text mb-0 mt-1 ml-8">
+            Cursos
+        </p>
+
+        <v-container class="d-flex justify-space-around pb-0">
             <CourseCard />
             <CourseSoonCard />
         </v-container>
         
     </v-container>
-
-   
-    
 </template>
 
 <script>
