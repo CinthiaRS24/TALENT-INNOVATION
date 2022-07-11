8a6e3def8bbeafb70e532039fcc6647dc4653bfb
Cinthia Ramos <cinthia24027@gmail.com>
2022-07-10 21:29:58 -0500

    Improve styles and fix code indentation in MentorsSection

--- src/components/MentorsSection.vue
+++ src/components/MentorsSection.vue
@@ -23,21 +23,36 @@
                 >
             </v-avatar>
 
-            <p class="title white--text font-weight-light mb-0">{{mentorSelected.name}}</p>
+            <p class="title white--text font-weight-light mb-0">
+                {{mentorSelected.name}}
+            </p>
 
-            <p class="subtitle-2 white--text font-weight-light mb-3">{{mentorSelected.role}}</p>
+            <p class="subtitle-2 white--text font-weight-light mb-3">
+                {{mentorSelected.role}}
+            </p>
 
-            <p class="subtitle-2 text-start ml-4 white--text font-weight-light mb-0">Habilidades</p>
+            <p class="subtitle-2 text-start ml-4 white--text font-weight-light mb-0">
+                Habilidades
+            </p>
+
             <v-container class="d-flex justify-space-around pb-4 pt-2">
-                <div class="d-flex" v-for="(skill, index) in mentorSelected.skills" 
+                <div 
+                    class="d-flex" v-for="(skill, index) in mentorSelected.skills" 
                     v-bind:key="'mentor-' + index"
                 >
-                    <v-btn small color="#d7b965" class="text-capitalize rounded-lg" style="font-size:11px;">{{skill}}</v-btn>
+                    <v-btn 
+                        small 
+                        color="#d7b965" 
+                        class="text-capitalize rounded-lg" 
+                        style="font-size:11px;"
+                    >
+                        {{skill}}
+                    </v-btn>
                 </div>
             </v-container>
         </v-card>
 
-        <p class="subtitle white--text font-weight-light ml-4 mt-6 mb-0">
+        <p class="subtitle white--text font-weight-light ml-4 mt-6 mb-0" v-if="mentorSelected">
             Más mentores
         </p>
 
@@ -46,17 +61,28 @@
             v-bind:key="'mentor-' + index"
             class="pb-1"
         >
-            <v-btn color="#23436b" width="100%" height="65" class="rounded-lg justify-end" @click="goToChat(mentor)">
+            <v-btn 
+                color="#23436b" 
+                width="100%" 
+                height="65" 
+                class="rounded-lg justify-end" 
+                @click="goToChat(mentor)"
+            >
                 <div>
-                    <span class="subtitle-1 white--text text-capitalize font-weight-light text-end pr-4">{{ mentor.name }}</span>
-                    <p class="subtitle-2 white--text text-capitalize font-weight-light text-end pr-4 mb-0">Mentor</p>
+                    <span class="subtitle-1 white--text text-capitalize font-weight-light text-end pr-4">
+                        {{ mentor.name }}
+                    </span>
+                    <p class="subtitle-2 white--text text-capitalize font-weight-light text-end pr-4 mb-0">
+                        Mentor
+                    </p>
                 </div>
 
                 <div class="mr-3">
                     <v-avatar size="50px">
                         <img
-                        :src="mentor.photoURL"
-                        alt="Avatar" />  
+                            :src="mentor.photoURL"
+                            alt="Avatar" 
+                        />  
                     </v-avatar>
                 </div>
             </v-btn>
@@ -69,8 +95,7 @@ import firebase from 'firebase/compat/app';
 
 export default {
     name: 'MentorsSection',
-    mounted() {
-        console.log('MentorsSection mounted', this.$route.params.mentorUID);        
+    mounted() {      
         this.fetchMentors();
     },
     data() {
